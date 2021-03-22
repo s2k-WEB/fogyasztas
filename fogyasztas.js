2a18faa432e5b167871ace89125146e99c2e5cf6
smokey2k <datas2k@gmail.com>
2021-03-22 15:20:00 +0100

    9. pont

--- fogyasztas.js
+++ fogyasztas.js
@@ -1,12 +1,11 @@
 var km = "";
 var tankolas = "";
-
 var autok = {
     evjarat : "1980",
     automarka : "Saab",
-    // fullName : function() {
-    //   return this.firstName + " " + this.lastName;
-    // }
+    fogyaszt  : function() {
+      return `${this.evjarat} évjáratú ${this.automarka} fogyasztása: ${tankolas / km} liter/km.`;
+    }
   };
 
 do {
@@ -16,19 +15,6 @@ do {
 do {
     tankolas = prompt("Hány litert tankoltál ?");
 } while(!isNumber(tankolas) || tankolas <= 1 || tankolas > 50)
-
-
-
-
-
-
-window.onload = function() {document.getElementById("fogyi").innerHTML = `Fogyasztás: ${km} km. ${tankolas}`};
-
-// if (km <= 1000) {
-//     window.onload = function() {
-//         document.getElementById("fogyi").innerHTML = `Fogyasztás: ${km} km.`;
-//     };
-// }
 
 function isNumber(value) {
     var pattern = /^[0-9]+$/;
